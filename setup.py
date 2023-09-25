09f1e1fb420521fdbbd049104db8f24412d0041e
EmanuelCasta <emanuel.castaneda.cardona@gmail.com>
2023-09-25 13:04:18 -0500

    mejorando documentacion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='injector_api',
-    version='0.0.7',
+    version='0.0.9',
     packages=find_packages(),
     author='Emanuel Castañeda',
     author_email='emanuel.castaneda.cardona@gmail.com',
